fbca63d0f7d4c009daebef25354ff3f1ce61d046
jnpcoach-prog <jnpcoach@gmail.com>
2025-10-28 08:42:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const BREVO_API = "https://api.brevo.com/v3";
 const API_KEY = process.env.BREVO_API_KEY;
 
 // ✅ Radix auf 10 setzen (oder Number() verwenden)
-const DOI_TEMPLATE_ID = parseInt(process.env.BREVO_DLI_TEMPLATE_ID, 10);
+const DOI_TEMPLATE_ID = parseInt(process.env.BREVO_DLI_TEMPLATE_ID2, 10);
 const LIST_ID_SUPERFOOD = parseInt(process.env.BREVO_LIST_ID_SUPERFOOD, 10);
 
 // Danke-Seite NACH Formular-Absenden (Frontend-URL!)
